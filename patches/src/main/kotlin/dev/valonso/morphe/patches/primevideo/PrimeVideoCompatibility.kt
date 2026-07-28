15d1fffacaf39eed9c53726c37c8c2c5bcb96ab5
valonso <80985309+valonsodev@users.noreply.github.com>
2026-07-28 20:40:26 +0200

    fix: support all Prime Video versions

--- patches/src/main/kotlin/dev/valonso/morphe/patches/primevideo/PrimeVideoCompatibility.kt
+++ patches/src/main/kotlin/dev/valonso/morphe/patches/primevideo/PrimeVideoCompatibility.kt
@@ -1,7 +1,6 @@
 package dev.valonso.morphe.patches.primevideo
 
 import app.morphe.patcher.patch.ApkFileType
-import app.morphe.patcher.patch.AppTarget
 import app.morphe.patcher.patch.Compatibility
 
 internal val PRIME_VIDEO_COMPATIBILITY = Compatibility(
@@ -9,8 +8,4 @@ internal val PRIME_VIDEO_COMPATIBILITY = Compatibility(
     packageName = "com.amazon.amazonvideo.livingroom",
     apkFileType = ApkFileType.APKM,
     appIconColor = 0x00A8E1,
-    targets = listOf(
-        AppTarget("6.24.2+v15.5.0.300-allAbis"),
-        AppTarget("6.24.4+v16.0.0.103-allAbis"),
-    ),
 )
